c33063c11ed59dade2003341a7dc2aed3e639a0f
Alex Morega <alex@grep.ro>
2019-03-31 18:05:17 +0300

    Keep update checks, they are nice

--- cluster.py
+++ cluster.py
@@ -136,7 +136,6 @@ CONFIG.nomad = lambda: f'''\
 data_dir = "{PATH.nomad_var}"
 leave_on_interrupt = true
 leave_on_terminate = true
-disable_update_check = true
 
 addresses {{
   http = "{OPTIONS.nomad_address}"
