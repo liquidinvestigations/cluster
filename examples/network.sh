a552fa545ece279e85f5c220594cd1984844388b
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-04-27 20:59:38 +0300

    Fix example/network.sh and add TODOs

--- examples/network.sh
+++ examples/network.sh
@@ -23,6 +23,8 @@ BRIDGE_ADDRESS=10.66.60.1
 
 ip link delete $BRIDGE_NAME type bridge || echo 'nothing to delete'
 ip link add name $BRIDGE_NAME type bridge
-ip link set dev liquid-bridge up
-ip link set dev $EXTERNAL_INTERFACE up
+ip link set dev $BRIDGE_NAME up
 ip address add dev $BRIDGE_NAME $BRIDGE_ADDRESS/24
+
+# TODO: add bridge to external interface
+# TODO: firewall
