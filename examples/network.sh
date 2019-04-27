3cfa57b5cda9e2b912e93b225875d246cacb312f
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-04-27 20:00:37 +0300

    Example script to set up network (#19)
    * Example script to set up network
    
    * Add how-to to networking script
    
    * Reference network config in readme

---
+++ examples/network.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ex
+
+# Set up local bridge network
+#
+#       sudo ./network.sh
+#
+# Then edit `cluster.ini` and edit:
+#
+#       [nomad]
+#       interface = liquid-bridge
+#       address = 10.66.60.01
+#       advertise = 10.66.60.01
+#
+#       [vault]
+#       address = 10.66.60.01
+#
+#       [consul]
+#       address = 10.66.60.01
+
+BRIDGE_NAME=liquid-bridge
+BRIDGE_ADDRESS=10.66.60.1
+
+ip link delete $BRIDGE_NAME type bridge || echo 'nothing to delete'
+ip link add name $BRIDGE_NAME type bridge
+ip link set dev liquid-bridge up
+ip link set dev $EXTERNAL_INTERFACE up
+ip address add dev $BRIDGE_NAME $BRIDGE_ADDRESS/24
