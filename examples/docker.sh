0fd991ef6382391605fb8fabace267beb0dd7c6b
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-05-24 19:22:13 +0300

    parametrize cluster location

--- examples/docker.sh
+++ examples/docker.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -ex
 
-
 if [ -z "${NOMAD_CLIENT_INTERFACE}" ]; then
   export NOMAD_CLIENT_INTERFACE=liquid-bridge
 fi
 
-docker run --detach --restart always \
+HERE=$(dirname "$(dirname "$0")")
+
+docker run --detach \
+  --restart always \
   --name cluster \
   --volume /var/run/docker.sock:/var/run/docker.sock \
-  --volume /opt/cluster/var:/opt/cluster/var \
-  --volume /opt/cluster/etc:/opt/cluster/etc \
-  --volume /opt/cluster/cluster.ini:/opt/cluster/cluster.ini \
+  --volume $HERE/var:/opt/cluster/var \
+  --volume $HERE/etc:/opt/cluster/etc \
+  --volume $HERE/cluster.ini:/opt/cluster/cluster.ini:ro \
   --privileged \
   --net host \
   --env NOMAD_CLIENT_INTERFACE=$NOMAD_CLIENT_INTERFACE \
