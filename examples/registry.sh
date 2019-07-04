01d776452121cb17538a2bbb08a6c0e14260386f
Alex Morega <alex@grep.ro>
2019-07-04 12:42:06 +0300

    Fix `$HERE`

--- examples/registry.sh
+++ examples/registry.sh
@@ -3,12 +3,12 @@
 ## Run dockerd with the following options:
 # /usr/bin/dockerd -H fd:// --insecure-registry 10.66.60.1:6665 --registry-mirror http://10.66.60.1:6665 --registry-mirror https://registry-1.docker.io
 
-HERE=$(realpath "$(dirname "$(dirname "$0")")")
+cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
 docker run --detach \
   --restart always \
   --name registry \
-  --volume $HERE/registry-config.yml:/etc/docker/registry/config.yml \
-  --volume $HERE/../var/registry:/var/lib/registry \
+  --volume ./examples/registry-config.yml:/etc/docker/registry/config.yml \
+  --volume ./var/registry:/var/lib/registry \
   --publish 6665:5000 \
   registry:2
