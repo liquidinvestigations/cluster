129110efe24579f76b071e63309868b7994dd16e
Alex Morega <alex@grep.ro>
2019-07-05 19:29:09 +0300

    First delete the rules, then create them

--- examples/network.sh
+++ examples/network.sh
@@ -25,14 +25,15 @@ ip link add name $bridge_name type bridge
 ip link set dev $bridge_name up
 ip address add dev $bridge_name $bridge_address/24
 
-iptables -t nat -A PREROUTING \
-  -d $public_address -p tcp --dport 80 \
-  -j DNAT --to-destination $bridge_address
-iptables -t nat -A PREROUTING \
-  -d $public_address -p tcp --dport 443 \
-  -j DNAT --to-destination $bridge_address
+dnat_to_bridge_http="-d $public_address -p tcp --dport 80 -j DNAT --to-destination $bridge_address"
+dnat_to_bridge_https="-d $public_address -p tcp --dport 443 -j DNAT --to-destination $bridge_address"
 
-iptables -t nat -A POSTROUTING -o $bridge_name -j MASQUERADE
+iptables -t nat -D PREROUTING $dnat_to_bridge_http || /bin/true
+iptables -t nat -A PREROUTING $dnat_to_bridge_http
+iptables -t nat -D PREROUTING $dnat_to_bridge_https || /bin/true
+iptables -t nat -A PREROUTING $dnat_to_bridge_https
+
+echo iptables -t nat -A POSTROUTING -o $bridge_name -j MASQUERADE
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
