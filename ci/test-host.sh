95cf2aceac5b240912d7740aab9833f87ddb3549
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-07-13 18:27:28 +0300

    deb packages are now pre-installed on vm

--- ci/test-host.sh
+++ ci/test-host.sh
@@ -3,10 +3,12 @@
 id $(whoami)
 cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )"
 
-echo "installing dependencies"
-sudo apt-get update -yqq
-sudo apt-get install -yqq python3-pip python3-venv git curl unzip dnsutils iptables
-sudo pip3 install pipenv
+# The VM already has these installed:
+#echo "installing dependencies"
+#sudo apt-get update -yqq
+#sudo apt-get install -yqq python3-pip python3-venv git curl unzip dnsutils iptables
+#apt-get install -yqq git python3 unzip docker.io supervisor python3-venv
+#sudo pip3 install pipenv
 pipenv --version
 pipenv install 2>&1
 
