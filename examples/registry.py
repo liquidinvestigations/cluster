df1ad828443caecbb54795cd1024a213c68db346
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-17 00:13:48 +0300

    registry_address -> registry_ip

--- examples/registry.py
+++ examples/registry.py
@@ -7,11 +7,11 @@ daemon = {}
 daemon['registry-mirrors'] = ['https://registry-1.docker.io']
 daemon['insecure-registry'] = []
 
-registry_address = os.environ.get("REGISTRY_ADDRESS")
+registry_ip = os.environ.get("REGISTRY_ADDRESS")
 registry_port = os.environ.get("REGISTRY_PORT")
 
-if registry_address and registry_port:
-    local_registry = f'{registry_address}:{registry_port}'
+if registry_ip and registry_port:
+    local_registry = f'http://{registry_ip}:{registry_port}'
     daemon['registry-mirrors'].insert(0, local_registry)
     daemon['insecure-registry'].insert(0, local_registry)
 
