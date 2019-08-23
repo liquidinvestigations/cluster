0577f8e88d13393711599d4110e6080e32e406bc
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-23 14:44:42 +0300

    Fix insecure-registry address

--- examples/registry.py
+++ examples/registry.py
@@ -9,11 +9,11 @@ daemon['insecure-registry'] = []
 
 registry_ip = os.environ.get("REGISTRY_ADDRESS")
 registry_port = os.environ.get("REGISTRY_PORT")
+registry_address = f'{registry_ip}:{registry_port}'
 
 if registry_ip and registry_port:
-    local_registry = f'http://{registry_ip}:{registry_port}'
-    daemon['registry-mirrors'].insert(0, local_registry)
-    daemon['insecure-registry'].insert(0, local_registry)
+    daemon['registry-mirrors'].insert(0, f'http://{registry_address}')
+    daemon['insecure-registry'].insert(0, registry_address)
 
 with open('daemon.json', 'w') as f:
     print(json.dumps(daemon, indent=2), file=f)
