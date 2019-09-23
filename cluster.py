7d3065d72181367edde207ac3f72e54f26574bcc
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-09-23 16:20:09 +0300

    remove obsolete cluster-fabio check

--- cluster.py
+++ cluster.py
@@ -603,7 +603,6 @@ HEALTH_CHECKS = {
     'vault': ['Vault Sealed Status'],
     'grafana': ['Grafana alive on HTTP'],
     'prometheus': ['Prometheus alive on HTTP'],
-    'cluster-fabio': ["Service 'cluster-fabio' check"],
 }
 
 
