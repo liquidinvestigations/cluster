349df8b0f0c20eb2102f8c6c328136bbcbfa3c12
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-09-03 10:24:34 +0300

    update all libraries and tools

--- cluster.py
+++ cluster.py
@@ -140,9 +140,9 @@ class OPTIONS:
         'nomad', 'drain_on_stop', fallback=True)
 
     versions = {
-        'consul': config.get('consul', 'version', fallback='1.8.1'),
-        'vault': config.get('vault', 'version', fallback='1.5.0'),
-        'nomad': config.get('nomad', 'version', fallback='0.12.1'),
+        'consul': config.get('consul', 'version', fallback='1.8.3'),
+        'vault': config.get('vault', 'version', fallback='1.5.3'),
+        'nomad': config.get('nomad', 'version', fallback='0.12.3'),
     }
 
     node_name = config.get('cluster', 'node_name',
