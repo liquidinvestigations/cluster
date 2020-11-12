5ff88f6fa9e6e9799e2ee20368757bdd3cb7734e
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-11-12 19:40:08 +0200

    fixup

--- ci/provision-vm.sh
+++ ci/provision-vm.sh
@@ -8,6 +8,7 @@ pip3 install pipenv
 echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.d/es.conf
 sysctl --system
 adduser vagrant docker
+adduser root docker
 chown vagrant: /opt
 
 mkdir /opt/cluster
@@ -16,9 +17,10 @@ tar xzf /opt/cluster.tar.gz
 cp ci/vm-cluster.ini .
 
 chown -R vagrant: .
-./bin/docker.sh
-docker exec cluster ./cluster.py wait
-docker stop -t 90 cluster
-docker ps
+
+sudo -nHu vagrant ./bin/docker.sh
+sudo -nHu vagrant docker exec cluster ./cluster.py wait
+sudo -nHu vagrant docker stop -t 90 cluster
+sudo -nHu vagrant docker ps
 
 echo "Provision done!"
