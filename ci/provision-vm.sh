4fee47ebe2104f10483d61fab05d563af56c1ec3
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-11-12 17:19:45 +0200

    do complete install when building qemu image

--- ci/provision-vm.sh
+++ ci/provision-vm.sh
@@ -3,10 +3,11 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -qq
 apt-get install -yqq git unzip supervisor docker.io python3-pip python3-venv
+pip3 install pipenv
+
 echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.d/es.conf
 sysctl --system
 adduser vagrant docker
-
 chown vagrant: /opt
 
 mkdir /opt/cluster
@@ -15,17 +16,9 @@ tar xzf /opt/cluster.tar.gz
 cp ci/vm-cluster.ini .
 
 chown -R vagrant: .
-
-docker pull liquidinvestigations/cluster
-cat > /etc/supervisor/conf.d/boot-vm.conf <<EOF
-[program:boot-vm]
-user = vagrant
-command = /opt/cluster/ci/boot-vm.sh
-redirect_stderr = true
-autostart = true
-autorestart = false
-EOF
-
-pip3 install pipenv
+./bin/docker.sh
+docker exec cluster ./cluster.py wait
+docker stop -t 90 cluster
+docker ps
 
 echo "Provision done!"
